7d3f6e6500695c0b00ee8681593133b49a28e929
Kerim Er <kerim._er@hotmail.com>
2022-10-16 01:32:34 +0300

    ilk yükleme

---
+++ README.md
@@ -0,0 +1,16 @@
+# Şifre Sorgula
+
+Kullandığınız şifreleri hazır şifre dosyalarından çekilen şifreler arasında sorgulamak için aracı bir program.
+
+## Kurulum
+
+1. `git clone https://github.com/spsofme/sifre-sorgula.git`
+2. `cd sifre-sorgula`
+3. `pip install -r requirements.txt`
+
+## Programın kullanımı
+
+`python3 ./sorgula.py`
+
+## Kullanılan şifre dosyaları
+[https://github.com/utkusen/turkce-wordlist/blob/master/wordlist.txt](https://github.com/utkusen/turkce-wordlist/blob/master/wordlist.txt)
---
+++ sorgula.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL_LIST = [
+	"https://github.com/utkusen/turkce-wordlist/blob/master/wordlist.txt"
+]
+
+password = input("sifrenizi girin: ")
+
+for _url in URL_LIST:
+	page = requests.get(url=_url)
+	soup = BeautifulSoup(page.content, "html.parser")
+	results = soup.find(class_="highlight tab-size js-file-line-container js-code-nav-container js-tagsearch-file")
+	if not results:
+		split_list = _url.split("github.com")
+		_url = split_list[0] + "raw.githubusercontent.com" + split_list[1]
+		split_list = _url.split("/blob/")
+		_url = split_list[0] + "/" + split_list[1]
+
+		page = requests.get(url=_url)
+		soup = BeautifulSoup(page.content, "html.parser")
+		results = True if str(soup).strip().find("!cetin") != -1 else False
+
+		print(results)
+		if (results): break
+	
+
+	results = True if results.find("td", string="ademelmasi") else False
+	print(results)
+	if (results): break
